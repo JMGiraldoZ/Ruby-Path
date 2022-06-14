6f8b4f7abfb744fb0c0dcc183f7ebaed8888886c
JMGiraldoZ <juan.miguel.giraldoz@gmail.com>
2022-06-14 10:35:12 -0500

    Add first ten ruby exercise

---
+++ RubyExercise/PrintExample.rb
@@ -0,0 +1,7 @@
+#Printing with puts
+puts "Hello"
+puts " World"
+
+#Printing with print
+print "Hello"
+print " World"
---
+++ RubyExercise/UserInput.rb
@@ -0,0 +1,3 @@
+puts "Hi tell us your nume"
+name = gets.chomp()
+puts "Hi "+ name + " wellcome to kommit"
